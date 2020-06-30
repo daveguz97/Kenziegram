4aebe403e6c4164ee23fe30e6b57771cdadaeffd
David Guzman <davidguzmaninc@gmail.com>
2020-06-29 20:24:24 -0400

    Added default express code

---
+++ kenziegram/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import multer from "multer";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, console.log(`Server running on port ${port}...`));
